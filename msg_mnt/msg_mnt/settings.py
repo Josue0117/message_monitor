beb634f20119b9d5c7e92f77d2777428d1788eac
Josue0117 <josue.h.r@outlook.com>
2021-05-02 01:10:07 -0500

    Navbar and Static folder/setup aded

--- msg_mnt/msg_mnt/settings.py
+++ msg_mnt/msg_mnt/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'msg_mnt.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['msg_mnt/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'msg_mnt/static'),)
