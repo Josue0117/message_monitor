b7c748ece1f2187ac1eedf7dfde414d40dbf6b32
Josue0117 <josue.h.r@outlook.com>
2021-05-02 06:01:04 -0500

    Navbar dinamic style and first version of login tab

--- msg_mnt/msg_mnt/views.py
+++ msg_mnt/msg_mnt/views.py
@@ -2,13 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return render(request,"index.html")
+    return render(request,"index.html",{"home":"active"})
 
 def mis_conversaciones(request):
-    return render(request,"index.html")
+    return render(request,"index.html",{"login":"active"})
 
 def sbr_conversaciones(request):
-    return render(request,"index.html")
+    return render(request,"index.html",{"login":"active"})
 
 def login(request):
-    return render(request,"login.html")
+    return render(request,"login.html",{"login":"active"})
