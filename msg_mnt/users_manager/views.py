994e9867d131cd30c1ec17bd6d6c1530d7070921
Josue0117 <josue.h.r@outlook.com>
2021-05-02 19:43:58 -0500

    templates folder and views.py migrated to the app

--- msg_mnt/users_manager/views.py
+++ msg_mnt/users_manager/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request,"index.html",{"home":"active"})
+
+def mis_conversaciones(request):
+    return render(request,"index.html",{"login":"active"})
+
+def sbr_conversaciones(request):
+    return render(request,"index.html",{"login":"active"})
+
+def login(request):
+    return render(request,"login.html",{"login":"active"})
