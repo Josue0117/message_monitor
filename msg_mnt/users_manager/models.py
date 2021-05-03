abb7eda2d456a6a16628ba474e1dbd6f7a1f79c1
Josue0117 <josue.h.r@outlook.com>
2021-05-02 19:35:45 -0500

    Second version of users_manager app created

---
+++ msg_mnt/users_manager/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+class User(models.Model):
+    user_name=models.CharField(max_length=50)
+    last_name=models.CharField(max_length=50)
+    nick_name=models.CharField(max_length=50)
+    user_email=models.EmailField(max_length=50)
+    password=models.CharField(max_length=50)
+    user_age=models.IntegerField()
+
+    ACADEMIC=1
+    RS_USER=2
+    VL_GROUP=3
+    ROL = (
+       (ACADEMIC, 'Academico'),
+       (RS_USER, 'Usuario S'),
+       (VL_GROUP, 'Grupo vulnerable'),
+    )
+    user_rol=models.PositiveSmallIntegerField(choices=ROL,default=ACADEMIC,)
+
+    MALE=1
+    FEMALE=2
+    OTHER=3
+    GENDER = (
+        (MALE, 'Hombre'),
+        (FEMALE, 'Mujer'),
+        (OTHER, 'Otro'),
+    )
+    user_gender=models.PositiveSmallIntegerField(choices=GENDER,default=FEMALE,)
